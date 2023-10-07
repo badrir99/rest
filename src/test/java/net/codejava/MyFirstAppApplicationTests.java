cb5ef3d15d58a54297f73f7514e516851662167d
badrir99 <badri_r99@yahoo.com>
2023-10-07 13:41:19 -0400

    test

---
+++ src/test/java/net/codejava/MyFirstAppApplicationTests.java
@@ -0,0 +1,9 @@
+package net.codejava;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+@SpringBootTest
+class MyFirstAppApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+}
